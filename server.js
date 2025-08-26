449b761353a8b50cddd9c5af91d6f074fa4f6dc9
Your Name <you@example.com>
2025-08-26 00:34:17 -0700

    Fix package.json; simple Express server

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// serve everything under /html
 app.use(express.static(path.join(__dirname, 'html')));
-
-// choose a homepage inside /html (change 'add.html' if you want another)
-app.get('/', (_, res) => res.sendFile(path.join(__dirname, 'html', 'add.html')));
+app.get('/', (_, res) => res.sendFile(path.join(__dirname, 'html', 'add.html'))); // change file if needed
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
